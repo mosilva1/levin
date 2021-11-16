a972ad71fca0bd5620fac95cf06014857f2111c1
mosilva1 <92989585+mosilva1@users.noreply.github.com>
2021-11-16 12:26:00 -0300

    item 1 - 3 - 4 - 6

--- js/login.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById("form-login");
-function realizaLogin(event) {
-  event.preventDefault();
-  alert("Seja Bem-Vindo!");
-  window.location.replace("levin.html");
-}
-form.addEventListener("submit", realizaLogin);
